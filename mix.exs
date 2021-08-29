1b6071e72eea783c97de3a821a1d465726b125d3
Kip Cole <kipcole9@gmail.com>
2021-08-29 11:23:04 +0800

    Simplify deps

--- mix.exs
+++ mix.exs
@@ -44,14 +44,12 @@ defmodule Cldr.Calendars.Ethiopic.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:ex_cldr_calendars, "~> 1.6"},
@@ -59,7 +57,6 @@ defmodule Cldr.Calendars.Ethiopic.MixProject do
       {:ex_doc, "~> 0.19", only: [:release, :dev]}
     ]
   end
-
 
   def links do
     %{
